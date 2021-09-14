6e27caa4a10070ab077f479d0cbac5e8702b2fe7
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-14 02:10:57 -0500

    added more content to HTML & CSS file

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// example Weather API call
+// api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335&units=imperial
+//http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}
