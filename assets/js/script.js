895f6c7b3b345417eb4865866e18c451012eebb6
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-20 01:25:16 -0500

    added eventListener to submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,43 @@
 
 //http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
 
-var cityName;
+var userFormEl = document.querySelector('#user-form');
+var userInput = document.querySelector('#city-search');
+var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "&units=imperial&appid=cd996b1bf4e8105324a410b704c9079d";
+// var apiURL2 = ;
 
+// var apiURL2 = *** GRAB SECOND URL, EXCLUDE MINUTE, HOURLY, ALERTS ***
+// *** FROM 1ST API GRAB THE LONGITUDE AND LATITUDE TO GET THE URL AS WELL ***
 
 // city parameter added to the function
 var getWeatherData = function (city) {
     // format the api url 
-    var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=cd996b1bf4e8105324a410b704c9079d";
+    var apiFetch = apiURL + city + apiKey;
 
     // make the request to the URL
-    fetch(apiURL).then(function(response) {
+    fetch(apiFetch).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getWeatherData ("");
+var submitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = userInput.value.trim();
+
+    if (cityName) {
+        getWeatherData(cityName);
+        userInput.value = '';
+    } else {
+        alert('Please enter a valid city!');
+    }
+
+    console.log(event);
+}
+
+
+userFormEl.addEventListener('submit', submitHandler);
+// getWeatherData ("salem");
